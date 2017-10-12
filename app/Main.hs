e75d3d478427b6c9a0a8c9c1a7f047d063e165f6
Jordan Medlock <jordanemedlock@gmail.com>
2017-10-12 18:57:45 -0400

    initial commit

---
+++ app/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Graphics.Implicit
+
+letter height 'J' = semi
+  where semi = difference [ring ir or, rectR 0 (-or, -or) (or, -or)]
+        width = height
+        strokeWidth = width / 10
+        innerDiameter = width/2 - strokeWidth
+        outterDiameter = width/2 + strokeWidth
+        (ir, or) = (innerDiameter/2, outterDiameter/2)
+
+
+ring ir or = difference [circle or, circle ir]
+
+plate w h d = rect3R 0 (0,0,0) (w, h, d)
+
+out = union [letter 40 'J']
+
+main = writeSVG 2 "test.svg" out
